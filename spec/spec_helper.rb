d82ca7e8edddc5365f5c9a62606a99bc25725736
Garrett Sand <garrettsand@gmail.com>
2015-09-11 18:45:01 -0700

    spec_helper.rb

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require "minitest/autorun"
+require "minitest/spec"
+
+# class FeatureSpec < Minitest::spec
+#   require "capybara/poltergeist"
+#   include Capybara::DSL
+#   Capybara.default_driver = :poltergeist
+#   register_spec_type(/page$/, self)
+# end
